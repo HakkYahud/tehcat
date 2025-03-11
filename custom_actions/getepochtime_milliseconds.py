7f71bf0dff59e96acfe368371b1ca26f0b27dc66
HakkYahud <108320420+HakkYahud@users.noreply.github.com>
2025-03-11 16:09:13 +0900

    Update getepochtime_milliseconds.py

--- custom_actions/getepochtime_milliseconds.py
+++ custom_actions/getepochtime_milliseconds.py
@@ -5,14 +5,14 @@ from tracecat_registry import registry
 
 
 @registry.register(
-    default_title="Get Epoch time in milliseconds",
+    default_title="Get Epoch time milliseconds",
     description="Convert date to a specific epoch time in milliseconds",
     display_group="Tehtris",
     namespace="integrations.tehtris",
 )
 
 def get_epoch_time(
-    timetosub: Annotated[int, Field(...,description="amount of time to substract")], unit: Annotated[str, Field(...,description="unit to substract (hours / minutes)")]
+    timetosub: Annotated[int, Field(default=1)], unit: Annotated[str, Field(...,description="unit to substract (hours / minutes)")]
     ) -> int:
     if unit == "hours":
         return int((datetime.now() - timedelta(hours=timetosub)).timestamp()*1000)
