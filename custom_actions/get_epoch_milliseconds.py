146846187858a691f19e00a3f7f45e9b0017eb07
HakkYahud <108320420+HakkYahud@users.noreply.github.com>
2025-03-11 16:11:56 +0900

    Create get_epoch_milliseconds.py

---
+++ custom_actions/get_epoch_milliseconds.py
@@ -0,0 +1,20 @@
+from typing import Annotated, Any, Literal
+from pydantic import Field
+from datetime import datetime, timedelta
+from tracecat_registry import registry
+
+
+@registry.register(
+    default_title="Get Epoch Time Milliseconds",
+    description="Convert date to a specific epoch time in milliseconds",
+    display_group="Tehtris",
+    namespace="integrations.tehtris",
+)
+
+def get_epoch_time(
+    timetosub: Annotated[int, Field(default=1)], unit: Annotated[str, Field(...,description="unit to substract (hours / minutes)")]
+    ) -> int:
+    if unit == "hours":
+        return int((datetime.now() - timedelta(hours=timetosub)).timestamp())
+    elif unit == "minutes":
+        return int((datetime.now() - timedelta(minutes=timetosub)).timestamp())
