7aa34336dc31a960816e5d5c968b55659f724813
HakkYahud <108320420+HakkYahud@users.noreply.github.com>
2025-07-15 08:10:06 +0200

    Add files via upload

---
+++ custom_actions/extract_tag.py
@@ -0,0 +1,16 @@
+from typing import Annotated, Any, Literal
+from pydantic import Field
+from tracecat_registry import registry
+
+@registry.register(
+    default_title="Extract Trigram",
+    description="Extract Trigram from TAG",
+    display_group="Utils",
+    namespace="integrations.utils",
+)
+
+def extract_tag(
+    tag: Annotated[str, Field(..., description="TAG of the affected system")],
+    ) -> str:
+    trigram = tag.split("_")[0]
+    return trigram
