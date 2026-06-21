0e4fbffe17d8a800a9796294516f787bc6ee6d66
HakkYahud <108320420+HakkYahud@users.noreply.github.com>
2026-06-21 19:10:47 +0900

    Create FindandReplace.py

---
+++ custom_actions/FindandReplace.py
@@ -0,0 +1,20 @@
+from typing import Annotated, Any, Literal
+from pydantic import Field
+from tracecat_registry import registry
+import re
+
+
+@registry.register(
+    default_title="Find and Replace",
+    description="Replace some character by another character",
+    display_group="Utils",
+    namespace="integrations.utils",
+)
+
+def sleep(
+    message: Annotated[str, Field(..., description="Message target to find the character to replace")],
+    char: Annotated[str, Field(..., description="Character to replace")],
+    newChar: Annotate[str, Field(..., description="New character")]
+    ) -> str:
+    
+    return message
