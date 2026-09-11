f951b1656bad004104e4094665c267f5a4a6decf
HakkYahud <108320420+HakkYahud@users.noreply.github.com>
2026-09-11 12:53:41 +0900

    Create FilterListDictionary.py

---
+++ custom_actions/FilterListDictionary.py
@@ -0,0 +1,21 @@
+from typing import Annotated, Any, Literal
+from pydantic import Field
+from tracecat_registry import registry
+
+@registry.register(
+    default_title="Filter list of dictionaries",
+    description="Get an item in the list of dictionaries",
+    display_group="Utils",
+    namespace="integrations.utils",
+)
+
+def filter_listDict(
+    list: Annotated[list, Field(..., description="list to filter")],
+    key: Annotated[str, Field(..., description="value to search")]
+    value: Annotated[str, Field(..., description="value to search")]
+    ) -> dict:
+
+    for item in list:
+      if item.get(key) == value
+
+    return item
