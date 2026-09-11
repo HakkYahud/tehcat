627293522d070254e71fba4094e391c1f9f69cf6
HakkYahud <108320420+HakkYahud@users.noreply.github.com>
2026-09-11 12:58:34 +0900

    Update FilterListDictionary.py

--- custom_actions/FilterListDictionary.py
+++ custom_actions/FilterListDictionary.py
@@ -11,8 +11,8 @@ from tracecat_registry import registry
 
 def filter_listDict(
     list: Annotated[list, Field(..., description="list to filter")],
-    key: Annotated[str, Field(..., description="value to search")]
-    value: Annotated[str, Field(..., description="value to search")]
+    key: Annotated[str, Field(..., description="value to search")],
+    value: Annotated[str, Field(..., description="value to search")],
     ) -> dict:
 
     for item in list:
